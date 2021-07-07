d7b55b295744a87c9616b28f8c2eddc9f8afed75
jjy0709 <48046928+jjy0709@users.noreply.github.com>
2021-07-07 16:55:45 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -1,64 +1,84 @@
 var AWS = require('aws-sdk');
 const fs = require('fs');
 var express = require('express');
+var bodyParser = require('body-parser');
 const { resolve } = require('path');
 const { rejects } = require('assert');
 const child_process = require('child_process');
+s3 = new AWS.S3({apiVersion: '2006-03-01'});
 
 var app = express();
 app.use(express.json());
+app.use(express.urlencoded({extended:false}));
 const port = 4000;
 
 var server = app.listen(port, function(){
     console.log("test server on port 4000");
 });
 
-app.post('/test', function(req,res){
-    var bucket = req.body.Bucket;
-	var key = req.body.Key;
-	var user_id =req.body.user_id;
-	var date = req.body.date;
-	var mp4Name = req.body.mp4_name;
-    var parserfileName = mp4Name.replace(".mp4", ".json")
-	var resultfileName = mp4Name.replace("gpmf.mp4", "result.json")
-    var resultName = req.body.result;
-    resultName = resultName.replace("result_filename", resultfileName);
-    console.log(key);
-    res.end(JSON.stringify({result:1}));
-    let file = fs.createWriteStream(mp4Name);
+app.get('/', function(req, res){
+    res.sendFile('home.html', {root: __dirname});
+});
 
-    s3 = new AWS.S3({apiVersion: '2006-03-01'});
-    var downloadParams = {Bucket: bucket, Key: key};
-
+app.get('/id', function(req,res){
+    req.setTimeout(60*4*1000)
+    var id = req.query.id;
+    var downloadParams = {Bucket:'s3-gpmf-test-bucket112905-dev', Key:id}
+    var mp4name = id.slice(id.lastIndexOf('/')+1);
+    // res.send("wait...");
+    let file = fs.createWriteStream(mp4name);
     s3.getObject(downloadParams).createReadStream()
-    .on('end', () => {
+    .on('end',()=>{
         return resolve();
     })
-    .on('error', (err) => {
+    .on('error', (err)=>{
         return rejects(err);
     }).pipe(file)
-    .on('close', () => {
-        const child1 = child_process.execFile("./gpmf-parser", [mp4Name, parserfileName], (err, stdin, stdout) => {
-            if(err) throw err;
-        })
-        const child2 = child_process.execFile("python3", ['./main.py', mp4Name], {maxBuffer:300*1024*1024}, (err, stdin, stdout) => {
-            if(err) throw err;
-            // const store_params = ['./store.py', jsonName, user_id, date, resultName, key]
-            // const child3 = child_process.execFile('python3', store_params, (err, stdin, stdout)=>{
-                // if(err) throw err;
-            var result = fs.readFile(resultfileName, (err, data)=>{
-                // var result = JSON.parse(data);
-                var uploadParams = {Bucket:"s3-gpmf-test-bucket112905-dev-dest", Key:resultName, Body:data}
-                // var uploadoptions = {partSize: 100 * 1024 * 1024, queueSize: 1};
-                s3.putObject(uploadParams, function(err, data){
-                    if(err) throw err;
-                    // res.end(JSON.stringify({result:1}));
-                    const child4 = child_process.execFile('rm', [mp4Name, parserfileName, resultfileName], (err, stdin, stdout)=>{
-                        if (err) throw err;
-                        console.log("Done!");
-                    });
-                })
+    .on('close', () =>{
+        const child1 = child_process.execFile("python3", ['./main.py', mp4name], {maxBuffer:300*1024*1024}, function(err,stdout,stderr){
+            var resultfileName = mp4name.replace("gpmf.mp4", "result.json");
+            // res.redirect(`localhost:4000/data/?id=${resultfileName}`);
+            res.setHeader('Content-Disposition', `attachment; filename=${resultfileName}`);
+            fs.createReadStream(resultfileName).pipe(res)
+            var jsonfilename = mp4name.replace("mp4", "json");
+            const child = child_process.execFile("rm", [mp4name, resultfileName, jsonfilename], function(err,stdout,stderr){
+                if(err) throw err;
+                console.log("Done");
             })
-        });
-    });
-});
+        })
+    })
+})
+
+app.post('/id', function(req,res){
+    var id = req.body.id;
+    var params = {
+        Bucket: 's3-gpmf-test-bucket112905-dev',
+        Prefix: 'public/' + id
+    };
+    s3.listObjectsV2(params, function(err, data){
+        if(err) throw err;
+        var list = `<ul>`;
+        var content = data.Contents;
+        var i = 0;
+        while(i < data.KeyCount){
+            if(content[i].Key.search("gpmf") != -1){
+                list = list + `<li><a href="/id/?id=${content[i].Key}">${content[i].Key}</a></li>`;
+            }
+            i = i+1;
+        }
+        list = list + `</ul>`;
+        var template = `
+        <!doctype html>
+        <html>
+            <head>MP4 list</head>
+            <body>
+                ${list}
+            </body>
+        </html>
+        <style>
+            li{ margin: 20px; }
+        </style>
+        `;
+        res.end(template);
+    })
+})
